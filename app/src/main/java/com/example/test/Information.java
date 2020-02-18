75d1ebc5766c239bdc6dc17b201d4b9e5e7d2c06
Sean O'Grady <sean.o.gradyus@gmail.com>
2020-02-18 10:27:16 +0000

    new gradient layouts

--- app/src/main/java/com/example/test/Information.java
+++ app/src/main/java/com/example/test/Information.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.TextView;
 
 public class Information extends AppCompatActivity {
 
     private Button button;
+    TextView TextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +23,12 @@ public class Information extends AppCompatActivity {
                 openHomePage();
             }
         });
+
+        TextView = findViewById(R.id.textView25);
+        String info = getBaseContext().getResources().getString(R.string.moreInfo);
+        TextView.append(info);
+
+
     }
     //Back to home button
     public void openHomePage(){
--- app/src/main/java/com/example/test/LearningActivity.java
+++ app/src/main/java/com/example/test/LearningActivity.java
@@ -3,13 +3,17 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LearningActivity extends AppCompatActivity {
-
+    TextView TextView;
     private Button button;
+    private Button toWebsite;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +26,22 @@ public class LearningActivity extends AppCompatActivity {
                 openHomePage();
             }
         });
+
+         toWebsite = findViewById(R.id.button50);
+
+        toWebsite.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                intent.setData(Uri.parse("https://amp.smh.com.au/lifestyle/health-and-wellness/lifting-weights-protects-the-brain-from-long-term-degeneration-20200210-p53ze3.html"));
+                startActivity(intent);
+            }
+        });
+
+        TextView = (TextView) findViewById(R.id.textView8);
+        String stStrategy22 = getBaseContext().getResources().getString(R.string.learnMore);
+        TextView.append(stStrategy22);
     }
 
     //Go to home page
